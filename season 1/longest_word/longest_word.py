6314a8dd95c87ecd315cfb53c2e7e280ac8c36a8
unknown <ghazalabdi0@gmail.com>
2026-06-06 23:31:37 +0330

    longet word

--- season 1/longest_word/longest_word.py
+++ season 1/longest_word/longest_word.py
@@ -14,7 +14,7 @@ def find_longest_word(words):
     for word in words:
         if len(word) == longest_length:
             longest_words.append(word)
-    return print("the longest word(s) in your sentence is: ", *longest_words)
+    return print("the longest word(s) in your sentence is: ", *longest_words, f"({longest_length} letters)")
 
 #run the app
 get_sentence()
